c617a77b47e8e7a391643265dde04a2d84b6fb5a
jakwuh <jakwuh@yandex-team.ru>
2017-09-08 16:01:54 +0300

    Update build

--- gulpfile.js
+++ gulpfile.js
@@ -58,7 +58,7 @@ const paths = {
     dest: 'build/static/css'
   },
   copy: {
-    src: 'client/**/*.{svg,png,jpg,wav,mp3,mp4,json}',
+    src: 'client/**/*.{svg,png,jpg,wav,mp3,mp4,ogg,json}',
     dest: 'build/static'
   },
   postProcess: {
